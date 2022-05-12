04553737dbfb99d44c2b39955361cbd731667214
FerCastor <fernandacastor0@gmail.com>
2022-05-12 16:11:19 -0300

    remoção livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,20 +7,8 @@ public class PrimeiroPrograma {
 	public static void main(String[] args) {
 		
 		Gato gato = new Gato();
-		Livro livro = new Livro();
 		
-		System.out.println(gato);
-		System.out.println(livro);
-		
-		
-		/*double a = 2.4;
-		double b = 1.69;
-			
-		System.out.println("A soma de a + b é: " + (a+b));*/
-	}
-}
+		System.out.println(gato);		
 
-class Livro {
-	private String livro;
-	private String num_pag;
+	}
 }
